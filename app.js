035733f2553d54cb24e338d56a20b5446337bd1a
DevanshuRoy216287 <im.devanshuroy@gmail.com>
2025-08-30 00:13:15 +0530

    update: created an app with no modules

---
+++ app.js
@@ -0,0 +1,13 @@
+// local variables
+
+const Devanshu = "Devanshu"
+const doc = "TheDoctor"
+const shak = "Shyamak"
+
+const sayHi = (name) => {
+    console.log("Hi", name)
+}
+
+sayHi(Devanshu)
+sayHi(doc)
+sayHi(shak)
