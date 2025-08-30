9a5507c2c47f2de97632f127ee7e3b31dc27f59c
DevanshuRoy216287 <im.devanshuroy@gmail.com>
2025-08-30 23:58:08 +0530

    update: added fs module

---
+++ sync-fs-module.js
@@ -0,0 +1,19 @@
+// read and write files using fs module (built-in)
+const { readFileSync, writeFileSync, read } = require("fs")
+
+// read content inside file
+const first = readFileSync("./fodder/first.txt", "utf8")
+const second = readFileSync("./fodder/second.txt", "utf8")
+
+console.log(first, second)
+
+writeFileSync(
+    "./fodder/writFileSync.txt",
+    "I just wrote this file with write File Sync"
+)
+
+writeFileSync(
+    "./fodder/append.txt",
+    "Im gonna append this everytime",
+    {flag: "a"} // adds the same content everytime
+)
